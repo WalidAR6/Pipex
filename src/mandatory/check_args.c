6bae8cd1d297d308c062922bb2a411c0b36c081e
Walid Araissi <waraissi@e2r7p6.1337.ma>
2023-01-22 19:02:04 +0100

    fix mandatory protection and steal leaks

--- src/mandatory/check_args.c
+++ src/mandatory/check_args.c
@@ -6,7 +6,7 @@
 /*   By: waraissi <waraissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 17:54:56 by waraissi          #+#    #+#             */
-/*   Updated: 2023/01/20 23:56:27 by waraissi         ###   ########.fr       */
+/*   Updated: 2023/01/22 00:01:44 by waraissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,6 @@ void	check_last_arg(t_params *vars)
 	if (vars->outfile == -1)
 	{
 		ft_printf(2, "Error");
+		exit(1);
 	}
 }
--- src/mandatory/execution.c
+++ src/mandatory/execution.c
@@ -6,7 +6,7 @@
 /*   By: waraissi <waraissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 22:41:05 by waraissi          #+#    #+#             */
-/*   Updated: 2023/01/20 23:58:39 by waraissi         ###   ########.fr       */
+/*   Updated: 2023/01/22 18:57:59 by waraissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,18 @@ void	exec_cmd_2(t_params *vars, char **envp, char **cmd, int *fd)
 
 	id = fork();
 	if (id == -1)
-	{
-		ft_printf(2, "Error in fork.");
 		exit(1);
-	}
 	if (id == 0)
 	{
-		close(fd[1]);
-		dup2(fd[0], 0);
-		dup2(vars->outfile, 1);
+		if (close(fd[1]) == -1 || dup2(fd[0], 0) == -1 || close(fd[0]) == -1
+			||dup2(vars->outfile, 1) == -1)
+			exit(1);
 		execute_cmd(vars, envp, cmd);
-		close(fd[0]);
 	}
 	else
 	{
-		close(fd[0]);
+		if (close(fd[1]) == -1 || close(fd[0]) == -1)
+			exit(1);
 		waitpid(id, 0, 0);
 	}
 }
@@ -42,25 +39,19 @@ void	exec_all_cmd(t_params *vars, char **envp, char **cmd1, char **cmd2)
 	int	fds[2];
 	int	id;
 
-	pipe(fds);
+	if (pipe(fds) == -1)
+		exit(1);
 	id = fork();
 	if (id == -1)
-	{
-		ft_printf(2, "Error in fork.");
 		exit(1);
-	}
 	if (id == 0)
 	{
-		close(fds[0]);
-		dup2(vars->infile, 0);
-		dup2(fds[1], 1);
-		close(fds[1]);
+		if (close(fds[0]) == -1 || dup2(vars->infile, 0) == -1
+			|| dup2(fds[1], 1) == -1 || close(fds[1]) == -1)
+			exit(1);
 		execute_cmd(vars, envp, cmd1);
 	}
 	else
-	{
-		close(fds[1]);
 		exec_cmd_2(vars, envp, cmd2, fds);
-	}
 	waitpid(id, 0, 0);
 }
